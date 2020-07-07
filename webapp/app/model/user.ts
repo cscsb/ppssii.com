d677c6975c4d3c50bdef6bb2b61beb3312487c91
cscsb <49179157+cscsb@users.noreply.github.com>
2020-07-08 01:28:43 +0800

    frontend

---
+++ webapp/app/model/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    email: string;
+    username: string;
+    password: string;
+}
+
+export class RegisterUser extends User {
+    password_confirmation: string;
+}
+
+export class ResetPwd extends RegisterUser{
+    token: string;
+
+}
+
