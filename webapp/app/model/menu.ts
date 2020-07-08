3dbd8964720845bab5024ab51b5ef1a49976f380
cscsb <49179157+cscsb@users.noreply.github.com>
2020-07-08 22:21:56 +0800

    frontend

--- webapp/app/model/menu.ts
+++
@@ -1,9 +0,0 @@
-export class Menu {
-    name: string;
-    orderid?: number;
-    pid?: number;
-    uid?: number | string;
-    username?: string;
-    id?: number | string;
-    child?: Array<any>;
-}
--- webapp/app/model/move-item.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MoveItem } from './move-item';
-
-describe('MoveItem', () => {
-  it('should create an instance', () => {
-    expect(new MoveItem()).toBeTruthy();
-  });
-});
